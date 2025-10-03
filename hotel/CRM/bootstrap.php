d65c9dbdea93a2fbc83d5abbf779d6eefe1dbb41
rhyzel <ella@gmail.com>
2025-10-03 19:23:51 +0800

    hotel

--- hotel/CRM/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Simple bootstrap for CRM module
-require_once __DIR__ . '/lib/Database.php';
-require_once __DIR__ . '/lib/Guest.php';
-require_once __DIR__ . '/lib/GuestRepository.php';
-require_once __DIR__ . '/lib/GuestService.php';
-
-// You can expand this to a PSR-4 autoloader or composer in the future.
--- hotel/reservation/reservation_api/booking_management/time_test.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo "Current server time: " . date('Y-m-d H:i:s') . "<br>";
-echo "Server timezone: " . date_default_timezone_get() . "<br>";
-echo "Time formatted: " . date('Y-m-d h:i A') . "<br>";
-?>
